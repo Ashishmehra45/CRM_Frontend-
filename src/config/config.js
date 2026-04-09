e2ff735ea379aa4c57971e3becbdb98b3359449b
Ashish Mehra <ashish6266mehra@gmail.com>
2026-04-09 11:31:41 +0530

    new

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,6 @@
-
-const isLocal = window.location.hostname === "localhost";
-
-
-const BASE_URL = isLocal 
-    ? "http://localhost:5000/api" 
-    : "https://crm-back-end-q15d.onrender.com/api"; 
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "https://crm-back-end-q15d.onrender.com/api";
 
 export const API_URL = BASE_URL;
