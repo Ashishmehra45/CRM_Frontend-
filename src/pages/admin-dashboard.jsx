c6e8ec3283811bb2c879fed089995787bf9ec6b3
Ashish Mehra <ashish6266mehra@gmail.com>
2026-04-09 13:33:01 +0530

    logo add

--- src/pages/admin-dashboard.jsx
+++ src/pages/admin-dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import imglogo from "../assets/im global.png";
 import {
   Users,
   Briefcase,
@@ -186,12 +187,8 @@ const AdminDashboard = () => {
       <aside className="w-80 bg-[#0F172A] text-slate-300 flex flex-col fixed left-0 top-0 h-screen shadow-2xl z-50">
         <div className="p-10">
           <div className="flex items-center gap-3 mb-1">
-            <div className="w-10 h-10 bg-blue-600 rounded-xl flex items-center justify-center text-white shadow-lg shadow-blue-600/20">
-              <Layers size={22} />
-            </div>
-            <h1 className="text-2xl font-black tracking-tighter text-white">
-              IM<span className="text-blue-500">GLOBAL</span>
-            </h1>
+            <img  className="w-16 h-16 mt-[-30px]" src={imglogo} alt="IMGLOBAL Logo" />
+           
           </div>
           <p className="text-[10px] text-slate-500 uppercase font-black tracking-[0.3em] ml-1">
             Admin Panel
